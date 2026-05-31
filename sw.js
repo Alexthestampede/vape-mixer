89af0d5b4af100cf456063a28dcc0956120fd469
Alexthestampede <alexthestampede.ibook@gmail.com>
2026-05-31 02:39:11 +0200

    Fix PWA paths for GitHub Pages subdirectory

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
 const CACHE_NAME = 'vape-mixer-v1';
 const urlsToCache = [
-  '/',
-  '/index.html',
-  '/manifest.json'
+  './',
+  './index.html',
+  './manifest.json'
 ];
 
 // Install event - cache assets
